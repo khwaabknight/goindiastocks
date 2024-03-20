55adadb6ec48da9bba1c17725d6ec605216af54d
Aman Gupta <khwaabknight@gmail.com>
2024-03-21 00:12:44 +0530

    home page created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,25 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        richblue: {
+          5: "#ECF5FF",
+          25: "#C6D6E1",
+          50: "#A0B7C3",
+          100: "#7A98A6",
+          200: "#537988",
+          300: "#2D5A6A",
+          400: "#073B4C",
+          500: "#063544",
+          600: "#042E3B",
+          700: "#032833",
+          800: "#01212A",
+          900: "#1e375f",
+          950: "#142341",
+        },
+      }
     },
+    
   },
   plugins: [],
 };
